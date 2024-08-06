d2d27eda4426f280a0b9abd12f5d3422a9cb94b8
dev <dev.aravind@culturecreates.com>
2024-08-06 13:28:45 +0530

    Fixed an issue where some of the performances were not loading

--- src/main.rb
+++ src/main.rb
@@ -32,15 +32,18 @@ def get_entities(entityType)
       urls << entity['href']
     end
   end
+  add_url_sparql_file = File.read('./sparql/add_derived_from.sparql')
   graph = RDF::Graph.new
   urls.each do |entity_url|
     begin
       entity_url = entity_url.gsub(' ', '+')
       loaded_graph = RDF::Graph.load(entity_url)
+      sparql_file_with_url = add_url_sparql_file.gsub("subject_url", entity_url)
+      loaded_graph.query(SPARQL.parse(sparql_file_with_url, update: true))
       graph << loaded_graph
     rescue StandardError => e
       puts "Error loading RDF from #{entity_url}: #{e.message}"
-      break
+      next
     end
   end
   
