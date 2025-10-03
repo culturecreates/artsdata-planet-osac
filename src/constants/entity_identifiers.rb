be8d7a7edb16f9735f24054707124d1dce11e259
dev <dev.aravind@culturecreates.com>
2025-10-03 16:36:21 +0530

    fix(identifiers): updated outdated css identifiers

--- src/constants/entity_identifiers.rb
+++ src/constants/entity_identifiers.rb
@@ -1,6 +1,6 @@
 EntityIdentifiers = {
-  TOUR_DATES: "td a.button.btn.btn-primary.cbButton.cbNewButton",
-  APPEARANCES: "div.date a",
+  TOUR_DATES: "div.card.shadow-sm.w-100.d-flex.flex-column a",
+  APPEARANCES: "div.card-date a",
   EXHIBITION_DATES: "td a.button.btn.btn-primary.cbButton.cbNewButton",
   EXHIBITIONS: "div.com-content-article__body ul a"
 }
